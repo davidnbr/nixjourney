ae71d7aaa1b6934f2ecbeae327970babd0ffd2d8
Davidnbr <davidnbr_98@hotmail.com>
2025-04-23 12:06:44 -0500

    add flake.lock

--- project-simple-cli/flake.nix
+++ project-simple-cli/flake.nix
@@ -12,13 +12,18 @@
       pkgs = nixpkgs.legacyPackages.${system};
     in
     {
-    devShells.${system}.default = pkgs.mkShell {
-      buildInputs = [ pkgs.bashInteractive pkgs.jq pkgs.cowsay self.packages.${system}.default];
-      shellHook = ''echo "Welcome to the Cowsay Project environment!"'';
-    };
     packages.${system}.default = pkgs.writeShellScriptBin "cowsay-hello" ''
       #!${pkgs.bashInteractive}/bin/bash
       ./cowsay_hello.sh
       '';
+
+    devShells.${system}.default = pkgs.mkShell {
+      name = "SimpleCLI";
+      buildInputs = [ pkgs.bashInteractive pkgs.jq pkgs.cowsay self.packages.${system}.default];
+      shellHook = ''
+      export SHELL=${pkgs.bashInteractive}/bin/bash
+      echo "Welcome to the Cowsay Project environment!"
+      '';
+    };
   };
 }
