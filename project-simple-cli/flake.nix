3590e0e5b1c7eb900a7e6f4340eb6d685b677e94
Davidnbr <davidnbr_98@hotmail.com>
2025-04-24 13:21:18 -0500

    chore: add jq overlay to use 1.6 chore: add mkDerivation to create a cowsay-hello pkg

--- project-simple-cli/flake.nix
+++ project-simple-cli/flake.nix
@@ -3,22 +3,46 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs-old.url = "github:NixOS/nixpkgs/22.11";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, nixpkgs-old }:
 
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+
+      oldpkgs = import nixpkgs-old { inherit system; };
+
+      overlay = final: prev: {
+        jq = oldpkgs.jq;
+      };
+      pkgs = import nixpkgs {
+        system = system;
+        overlays = [ overlay ];
+      };
     in
     {
-    packages.${system}.default = pkgs.writeShellScriptBin "cowsay-hello" ''
-      ./cowsay_hello.sh
-      '';
+
+    overlays.default = overlay;
+
+    packages.${system}.default = pkgs.stdenv.mkDerivation {
+        name = "cowsay-hello";
+        buildInputs = [ pkgs.cowsay ];
+
+        src = ./.;
+
+        installPhase = ''
+          cowsay --version || exit 1
+          mkdir -p $out/bin/
+          cp cowsay_hello.sh $out/bin/cowsay_hello
+          chmod +x $out/bin/cowsay_hello
+          '';
+
+      };
 
     devShells.${system}.default = pkgs.mkShell {
       name = "SimpleCLI";
-      buildInputs = [ pkgs.bashInteractive pkgs.jq pkgs.cowsay self.packages.${system}.default];
+      buildInputs = [ pkgs.bashInteractive pkgs.cowsay pkgs.jq self.packages.${system}.default];
       shellHook = ''
         echo "Welcome to the Cowsay Project environment!"
         echo
